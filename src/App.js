aa9252066200cf68cdd1c10f108aea0de4aa67dc
Gustavo <gustavo_roger@yahoo.com>
2023-02-13 19:44:49 -0800

    suspense assignment complete

--- src/App.js
+++ src/App.js
@@ -1,58 +1,45 @@
-import { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css';
+import { useState, Suspense, useEffect } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
-import AlbumView from './components/AlbumView'
-import ArtistView from './components/ArtistView'
-import { DataContext } from './context/DataContext'
+import Spinner from './images/1_9EBHIOzhE1XfMYoKz1JcsQ.gif'
+import { createResource as fetchData } from './helper'
 
 function App() {
-	let [search, setSearch] = useState('')
-	let [message, setMessage] = useState('Search for Music!')
-	let [data, setData] = useState([])
+  let [searchTerm, setSearchTerm] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState(null)
 
-	const API_URL = 'https://itunes.apple.com/search?term='
+  useEffect(() => {
+    if (searchTerm) {
+      document.title=`${searchTerm} Music`
+      console.log(fetchData(searchTerm))
+      setData(fetchData(searchTerm))
+  }
+  }, [searchTerm])
 
-	useEffect(() => {
-		if(search) {
-			const fetchData = async () => {
-				document.title = `${search} Music`
-				const response = await fetch(API_URL + search)
-				const resData = await response.json()
-				if (resData.results.length > 0) {
-					return setData(resData.results)
-				} else {
-					return setMessage('Not Found')
-				}
-			}
-			fetchData()
-		}
-	}, [search])
-	
-	const handleSearch = (e, term) => {
-		e.preventDefault()
-		setSearch(term)
-	}
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearchTerm(term)
+  }
 
-	return (
-		<div>
-		{message}
-			<Router>
-				<Routes>
-					<Route path="/" element={
-						<div>
-							<SearchBar handleSearch = {handleSearch}/>
-							<DataContext.Provider value={data}>
-								<Gallery data={data} />
-							</DataContext.Provider>
-						</div>
-					} />
-					<Route path="/album/:id" element={<AlbumView />} />
-					<Route path="/artist/:id" element={<ArtistView />} />
-				</Routes>
-			</Router>
-		</div>
-	)	
+  const renderGallery = () => {
+    if(data){
+      return (
+        <Suspense fallback={<Spinner />}>
+          <Gallery data={data} />
+        </Suspense>
+      )
+    }
+  }
+
+  return (
+    <div className="App">
+      <SearchBar handleSearch={handleSearch} />
+      {message}
+      {renderGallery()}
+    </div>
+  );
 }
 
 export default App;
